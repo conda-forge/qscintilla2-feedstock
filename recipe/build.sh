f48e4eae19a7a69e6d105e5c7a2de49fa8c9e8db
Alexandre Neto <senhor.neto@gmail.com>
2023-01-06 09:57:54 +0000

    Revert "Try add qt-shared"
    This reverts commit fcdaa7b7a1fa712c7184ccaeebc48d42a26dd012.

--- recipe/build.sh
+++ recipe/build.sh
@@ -71,11 +71,8 @@ if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
   EXTRA_FLAGS="--target-dir $SITE_PKGS_PATH"
 fi
 
-echo $PATH
-
 $SIP_COMMAND \
     --no-make \
-	--qt-shared \
     --qsci-features-dir ../src/features \
     --qsci-include-dir ../src \
     --qsci-library-dir ../src \
